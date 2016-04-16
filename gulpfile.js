02d8f2ae82d8fa986870c24836a350e449e46433
shallaa <shallaa@shallaazm.com>
2016-04-16 09:23:10 +0000

    빌드 세팅

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,7 @@ var gulp = require('gulp');
 
 require('require-all')(path.resolve(__dirname, 'build'));
 
+gulp.task('build', ['build:sass', 'build:js', 'build:html']);
+gulp.task('watch', ['watch:sass', 'watch:js', 'watch:html']);
 
-
-// gulp.task('build', ['build:js', 'build:sass', 'build:image', 'build:html']);
-
-// gulp.task('watch', ['watch:js', 'watch:sass', 'watch:html']);
-
-// gulp.task('default', ['build', 'watch']);
+gulp.task('default', ['watch', 'server']);
